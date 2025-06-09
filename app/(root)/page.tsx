2c6290c0ac455e83413ea302da31197d09824dca
builtbywilldev <builtbywill@proton.me>
2025-06-09 14:51:27 -0600

    interview card styled and ready to implement into the DB!

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import InterviewCard from '@/components/InterviewCard'
 import { Button } from '@/components/ui/button'
+import { dummyInterviews } from '@/constants/index'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -24,14 +26,19 @@ const page = () => {
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
         <div className="interviews-section">
-          <p>You haven't taken any interviews yet</p>
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {... interview} key={interview.id}/>
+          ))}
+          {/* <p>You haven't taken any interviews yet</p> */}
         </div>
       </section>
 
       <section className="flex flex-col gap-6 mt-8">
         <h2>Take an Interview</h2>
         <div className="interviews-section">
-          <p>There are no interviews available</p>
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {... interview} key={interview.id}/>
+          ))}
         </div>
       </section>
       
